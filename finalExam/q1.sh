e2cbf032f58669ab76e0b476075439bd8c71b4f2
Takeharu.Oshida <georgeosddev@gmail.com>
2013-05-15 01:01:57 +0900

    add finalExam | q1,q2,q3

---
+++ finalExam/q1.sh
@@ -0,0 +1,4 @@
+#/usr/bin sh
+# mongorestore --collection messages --db enron dump/enron/messages.bson
+
+mongo < q1.txt
---
+++ finalExam/q2.sh
@@ -0,0 +1,5 @@
+#/usr/bin sh
+# mongorestore --collection messages --db enron dump/enron/messages.bson
+
+mongo < q2.txt
+mongo < q2_validate.txt
